649e18719670112ff6659c32908c8d094d04a896
예린쓰 <32544102+h2002y@users.noreply.github.com>
2023-12-14 13:17:37 +0900

    [Bronze V] Title: Zadanie próbne, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string input;
+    cin >> input;
+    for(int i = input.length() - 1; i >= 0; --i)
+        cout << input[i];
+    return 0;
+}
