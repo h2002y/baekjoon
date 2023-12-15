2750673d11ac43994c65f64514cb52feb558fe01
예린쓰 <32544102+h2002y@users.noreply.github.com>
2023-12-15 18:04:52 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string id;
+    cin >> id;
+    cout << ":fan::fan::fan:\n:fan::" << id << "::fan:\n:fan::fan::fan:\n";
+    return 0;
+}
