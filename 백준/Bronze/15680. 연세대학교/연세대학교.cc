2559261204ba2245fa1491c83975b287be8efe2d
예린쓰 <32544102+h2002y@users.noreply.github.com>
2023-12-15 18:24:42 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool yonsei;
+    cin >> yonsei;
+    if(yonsei) cout << "Leading the Way to the Future\n";
+    else cout << "YONSEI\n";
+    return 0;
+}
