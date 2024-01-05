abda5a0fdae691500fdead9c6846d422e5d83b04
예린쓰 <32544102+h2002y@users.noreply.github.com>
2024-01-05 21:52:55 +0900

    [Silver III] Title: 소수의 자격, Time: 36 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/6219. 소수의 자격/소수의 자격.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main () {
+    int A, B, D, cnt = 0;
+    cin >> A >> B >> D;
+    vector<int> isPrime(B + 1, true);
+    isPrime[1] = false;
+    for (int i = 2; i * i <= B; ++i) {
+        if(!isPrime[i]) continue;
+        for (long long j = i * i; j <= B; j = j + i)
+            isPrime[j] = false;
+    }
+    for (int i = A; i <= B; ++i) {
+        if (isPrime[i]) {
+            int num = i;
+            while (num != 0) {
+                if (num % 10 == D) {
+                    ++cnt;
+                    break;
+                }
+                num /= 10;
+            }
+        }
+    }
+    cout << cnt << "\n";
+    return 0;
+}
