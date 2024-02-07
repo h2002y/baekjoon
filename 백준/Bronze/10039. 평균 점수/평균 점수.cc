8dff332be03e373f8302b0ccd20a8e9a6aad528a
예린쓰 <32544102+h2002y@users.noreply.github.com>
2024-02-07 17:43:47 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int sum = 0;
+    for (int i = 0; i < 5; ++i) {
+        int num;
+        cin >> num;
+        if (num < 40) sum += 40;
+        else sum += num;
+    }
+    cout << sum / 5 << "\n";
+    return 0;
+}
