5848e8650b6014535e0b95df0f0eb58817c87961
예린쓰 <32544102+h2002y@users.noreply.github.com>
2024-03-08 15:36:36 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.cc
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.cc
@@ -1,20 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  int N, answer = 0;
-  cin >> N;
-
-  for (int c1 = 1; c1 < N; ++c1) {
-    for (int c2 = c1 + 2; c2 < N; ++c2) {
-      if(c1 + c2 >= N) break;
-      int c3 = (N - (c1 + c2));
-      if(c3 % 2 == 1) continue;
-      if(c1 + c2 + c3 == N) 
-        ++answer;
+int main()
+{
+    int N, cnt = 0;
+    cin >> N;
+    for (int i = 1; i < N - 1; ++i)
+    {
+        for (int j = i + 2; j < N - i - 1; ++j)
+        {
+            int z = N - i - j;
+            if (z % 2 == 0)
+                ++cnt;
+        }
     }
-  }
-
-  cout << answer << "\n";
-  return 0;
+    cout << cnt << "\n";
+    return 0;
 }
