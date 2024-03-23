ace7f3d4b58d4c4237222a59fb75ea392eb48358
예린쓰 <32544102+h2002y@users.noreply.github.com>
2024-03-23 23:15:05 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main () {
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i) {
+        string s;
+        cin >> s;
+        reverse(s.begin(), s.end());
+        cout << s << "\n";
+    }
+    return 0;
+}
