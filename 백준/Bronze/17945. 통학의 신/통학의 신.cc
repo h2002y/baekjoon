b8c479e73ac5d7c7395ab6553924742ed929840f
예린쓰 <32544102+h2002y@users.noreply.github.com>
2024-04-26 22:51:29 +0900

    [Bronze III] Title: 통학의 신, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.cc
+++ 백준/Bronze/17945. 통학의 신/통학의 신.cc
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main () {
     int A, B;
     cin >> A >> B;
     for (int i = -1000; i <= 1000; ++i)
-    {
         if (i * i + 2 * A * i + B == 0)
             cout << i << " ";
-    }
     cout << "\n";
     return 0;
 }
